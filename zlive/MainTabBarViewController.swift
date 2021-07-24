6a04ddecf2f955efb3dc5f44d4fe01f0a5b5d669
zhangzongyu <zhangzongyu@bytedance.com>
2021-07-24 13:10:54 +0800

    添加tabbarItem图片

--- zlive/MainTabBarViewController.swift
+++ zlive/MainTabBarViewController.swift
@@ -11,9 +11,25 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let homeVC : HomeViewController = HomeViewController()
-        homeVC.tabBarItem = UITabBarItem.init(title: "首页", image: nil, selectedImage: nil)
+        let homeImg : UIImage? = UIImage.init(named: "首页.png")
+        homeVC.tabBarItem = UITabBarItem.init(title: "首页", image: setTabBarItemImg(img: homeImg), selectedImage: nil)
         let meVC : MeViewController = MeViewController()
-        meVC.tabBarItem = UITabBarItem.init(title: "我的", image: nil, selectedImage: nil)
+        let meImg : UIImage? = UIImage.init(named: "我的.png")
+        meVC.tabBarItem = UITabBarItem.init(title: "我的", image: setTabBarItemImg(img: meImg), selectedImage: nil)
         self.viewControllers = [homeVC, meVC]
+    }
+}
+
+// tabBarItem相关
+extension MainTabBarViewController {
+    // 图片过大，需要 切图 才能在区域内放得下
+    func setTabBarItemImg(img:UIImage?) -> UIImage? {
+        guard img != nil else { return nil }
+        let size : CGSize = CGSize.init(width: 30, height: 30)
+        UIGraphicsBeginImageContext(size)
+        img?.draw(in: CGRect.init(x: 0, y: 0, width: size.width, height: size.height))
+        let newImg : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImg
     }
 }
