a8ef15518ea7660c1cbdb8e61c98f44257e9990e
zhang <zhangzongyu3518@icloud.com>
2021-07-27 02:06:50 +0800

    首页添加一个UITableView进行数据展示

--- zlive/HomePage/HomeViewController.swift
+++ zlive/HomePage/HomeViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
+    // 添加一个UITableView
+    private lazy var tableView : UITableView = {
+        let view = UITableView()
+        view.backgroundColor = UIColor.gray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         
         self.title = "首页"
+        self.configUI()
+    }
+}
+
+extension HomeViewController {
+    func configUI() {
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.left.top.right.equalTo(self.view)
+            make.height.equalTo(800)
+        }
     }
 }
